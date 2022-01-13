a0f42c3a7d4fb75fafb4a8bacd3c5d7d22684f11
HasAzez <hasan.boreny00@gmail.com>
2022-01-13 21:23:57 +0200

    This is test from IntelliJ

---
+++ src/main/java/JsonCollection.java
@@ -0,0 +1,105 @@
+import com.google.common.base.Stopwatch;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+public class JsonCollection {
+    private final ConcurrentHashMap<String, JsonObject> uniqueIndexedMap;
+    private final ConcurrentHashMap<String, ArrayList<JsonObject>> propertyIndexedMap;
+    private final String name;
+    private boolean isPropertyIndexed;
+    private String indexProperty;
+    private int count;
+
+
+    public JsonCollection(String name) {
+        this.propertyIndexedMap = new ConcurrentHashMap<>();
+        this.uniqueIndexedMap = new ConcurrentHashMap<>();
+        this.name = name;
+        count = 0;
+    }
+
+
+    public void insert(String jsonSentence) {
+
+        String wrapped = wrapID(jsonSentence);
+        JsonParser jsonParser = new JsonParser();
+        JsonObject uniqueIndexedJson = jsonParser.parse(wrapped).getAsJsonObject();
+        uniqueIndexedMap.put(DigestUtils.sha1Hex(count + ""), uniqueIndexedJson);
+        if (isPropertyIndexed)
+            insertToPropertyMap(uniqueIndexedJson);
+        count++;
+
+    }
+
+    private String wrapID(String jsonSentence) {
+        String hashedIndex = DigestUtils.sha1Hex(count + "");
+        return "{\"_id\":\"" + hashedIndex + "\"," + jsonSentence.substring(1);
+    }
+
+
+    public boolean deleteUsingID(String key) {
+        if (uniqueIndexedMap.isEmpty() || uniqueIndexedMap.get(key) == null)
+            return false;
+
+        if (isPropertyIndexed) {
+            String specificProperty = uniqueIndexedMap.get(key).get(indexProperty).getAsString();
+
+            ArrayList<JsonObject> wantedBucket = propertyIndexedMap.get(specificProperty);
+            wantedBucket.removeIf(jo -> jo.get("_id").getAsString().equals(key));
+        }
+        uniqueIndexedMap.remove(key);
+
+
+        count--;
+
+        return true;
+    }
+
+    public Collection<JsonObject> getAll() {
+        return uniqueIndexedMap.values();
+    }
+
+    public Iterator<JsonObject> getCertainFromIndexed(String searched) throws NoSuchFieldException {
+        if (isPropertyIndexed) {
+            return propertyIndexedMap.get(searched).iterator();
+        } else {
+            throw new NoSuchFieldException();
+        }
+
+    }
+
+    public void makeIndexed(String indexProperty) {
+        setIndexProperty(indexProperty);
+        boolean haveValuesCondition = !isPropertyIndexed && !uniqueIndexedMap.isEmpty();
+        if (haveValuesCondition) {
+            for (JsonObject f :
+                    uniqueIndexedMap.values()) {
+                insertToPropertyMap(f);
+            }
+        }
+        isPropertyIndexed = true;
+
+    }
+
+    public boolean isEmpty() {
+        return uniqueIndexedMap.isEmpty();
+    }
+
+    private void insertToPropertyMap(JsonObject f) {
+        String index = f.get(indexProperty).getAsString();
+        boolean checkIfWereBefore = propertyIndexedMap.get(index) == null;
+        if (checkIfWereBefore) {
+            propertyIndexedMap.put(index, new ArrayList<>());
+        }
+        propertyIndexedMap.get(index).add(f);
+    }
+
+    public void setIndexProperty(String indexProperty) {
+        this.indexProperty = indexProperty;
+    }
+}
