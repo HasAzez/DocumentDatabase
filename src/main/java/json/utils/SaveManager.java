df8f6e69fbc760411ada01cfd162a5e13b19ddec
HasAzez <hasan.boreny00@gmail.com>
2022-01-29 14:16:16 +0200

    class have generic static methods to serialize objects

---
+++ src/main/java/json/utils/SaveManager.java
@@ -0,0 +1,25 @@
+package json.utils;
+
+import java.io.*;
+
+public class SaveManager {
+
+
+    private SaveManager() {
+    }
+
+    public static <T extends Serializable> void save(T object, String name) throws IOException {
+        try (FileOutputStream fileOutputStream = new FileOutputStream("database.db");
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
+            objectOutputStream.writeObject(object);
+        }
+    }
+
+
+    public static <E> E load(File file) throws IOException, ClassNotFoundException {
+        try(FileInputStream fileInputStream = new FileInputStream("database.db");
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
+            return (E) objectInputStream.readObject();
+        }
+    }
+}
