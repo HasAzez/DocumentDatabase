f95eedc4e6f909e6fdd141d8f4073c71f39a5c34
HasAzez <hasan.boreny00@gmail.com>
2022-02-15 04:25:58 +0200

    Changed Some functions to obey the Strategy Pattern

---
+++ src/main/java/controller/BackgroundServer.java
@@ -0,0 +1,68 @@
+package controller;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public enum BackgroundServer {
+    INSTANCE(new ArrayList<>());
+
+    private final List<Node> users;
+
+    BackgroundServer(List<Node> users) {
+        this.users = users;
+    }
+
+
+    public <T extends Serializable>  void connect(ServerSocket serverSocket,T object) throws IOException {
+
+        Socket node =  serverSocket.accept();
+        Node connection = new Node(node);
+        connection.setPort(9000+ users.size()+"");
+        System.out.println("New node connected");
+        subscribe(connection);
+        connection.send(object);
+
+    }
+    public void subscribe(Node listener) {
+        users.add(listener);
+    }
+
+    public void unsubscribe(Node listener) {
+        users.remove(listener);
+    }
+
+    public <T extends Serializable> void broadcast(T object) throws IOException {
+
+        for (Node listener : users) {
+            listener.send(object);
+        }
+    }
+
+    public <T extends Serializable>  void runServer(T object) throws IOException {
+        ServerSocket nodesController = new ServerSocket(8989);
+        Runnable r = () -> {
+            try {
+                while (true)
+                    connect(nodesController,object);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+        new Thread(r).start();
+    }
+
+    public List<String> getPorts(){
+        List<String> ports = new ArrayList<>();
+        for(Node node:users){
+            ports.add(node.getPort());
+        }
+        return ports;
+    }
+
+    }
+
+
